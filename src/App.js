7017323c754e19d00d611090b74b624531d7b05e
Erick Villalobos Aleman <erickva70@gmail.com>
2019-06-16 22:43:28 -0500

    change view

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ class App extends Component {
 
   async componentWillMount(){
     try{
+      console.log('Si quieres ver mas utiliza Firefox y habilita en la consola el XHR para ver la peticion al API');
+      console.log('https://www.mozilla.org/es-MX/firefox/new/')
       const response = await this.fetchPhrase('https://api-helicon.herokuapp.com/api/v1/upperPhrase');
       console.log(response)
       this.setState({upperPhrase: response.result})
@@ -56,34 +58,30 @@ class App extends Component {
       <div className="App">
         <Container className="App-header">
           <form onSubmit={this.onSubmitChange} >
-            <Paper style={{ minWidth: 270, minHeight: 200, padding: 10 }} >
+            <Paper style={{ padding: 10, width: 500 }} >
                 <Grid container spacing={3} >
                   <Grid item xs={12} >
-                      <Typography color="primary" variant="h3" >Upper Phrases</Typography>
+                      <Typography color="primary" variant="h3" >Bloq Mayus</Typography>
                   </Grid>
                   <Grid item xs={12} >
-                      <TextField id="input-text" name="sendPhrase" label="phrase" margin="normal" variant="outlined" fullWidth onChange={this.handleChange} />
+                      <TextField id="input-text" name="sendPhrase" label="Ingresa una palabra para volverla MAYUSCULA" margin="normal" variant="outlined" fullWidth onChange={this.handleChange} />
+                  </Grid>
+                  <Grid item xs={12} >
+                    <input type='submit' id="submitForm" style={{ display: 'none' }} />
+                    <label htmlFor="submitForm" >
+                      <Button fullWidth variant="contained" component="span" color="secondary" >MAYUS</Button>
+                    </label>
                   </Grid>
                   <Grid item xs={12}>
                       <div>
                           <Typography color="primary" variant="h5" >{this.state.upperPhrase}</Typography>
                       </div>
                   </Grid>
-                  <Grid item xs={12} >
-                    <input type='submit' id="submitForm" style={{ display: 'none' }} />
-                    <label htmlFor="submitForm" >
-                      <Button variant="contained" component="span" >Enviar</Button>
-                    </label>
-                  </Grid>
-                  <Grid item xs={12} >
-                  </Grid>
-                  <Grid item xs={12}>
-                  </Grid>
                 </Grid>
             </Paper>    
           </form>
           <footer>
-            <span> I ❤️ <a href='https://github.com/ErickVAleman/ui-helicon' >GitHub</a></span>
+            <span>Codigo alojado con <span role="img" aria-label="heart" >❤️</span> en <a href='https://github.com/ErickVAleman/ui-helicon' >GitHub.com</a></span>
           </footer>
         </Container>
       </div>
